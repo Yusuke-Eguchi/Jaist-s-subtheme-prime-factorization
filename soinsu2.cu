7f53ce5f3bca6da9cb43c9ea4ef476751274350b
Yusuke-Eguchi <yusuke499@gmail.com>
2021-04-02 16:02:53 +0900

    更新

--- soinsu2.cu
+++ soinsu2.cu
@@ -3,36 +3,37 @@
 
 #define target 2*2*2*3*3*3*5*5*5
 
-__device__ void GCD(int *a, int *b)
+__device__ int GCD(int a, int b)
 {
-	if(*a = 0){
-		return *b;
-	}
-	else{
-		return GCD(b%a, a)
+	int c;	
+	if(a == 0){
+		return b;
+	} else if(a> b){
+		return GCD(b, a);
+	}else{
+		c = b % a;
+		return GCD(c, a);
 	}
 }
 
-__global__ void kernel(int *A)
+__global__ void kernel(int A)
 {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	int j = blockIdx.y * blockDim.y + threadIdx.y;
-	int a;
 	if(i >= 1 && j >= 1 && i > j){
-		if((i+__sqrt(*A))^2 % *A == (j + __sqrt(*A))^2 % *A &&){
-			a = GCD(i + __sqrt(*A), j + __sqrt(*A));
+		if((i+__sqrt(A))^2 % A == (j + __sqrt(A))^2 % A){
+			printf("%d ", GCD(i - j, A));
 		}
 	}
-retuen 0;
 }
 
 int main(){
-    int *d_target, A = target, a, b, i, j;
+    int *d_target, A = target;
     cudaMalloc((void**)&d_target,sizeof(int));
 	cudaMemcpy(d_target,&A,sizeof(int),cudaMemcpyHostToDevice);
 	dim3 block(32,32);
 	dim3 grid((A+31)/32, (A+31)/32);
-	(i, j, a, b) = kernel<<<grid,block>>>(d_target);
+	kernel<<<grid,block>>>(d_target);
 	cudaFree(d_target);
 	printf("\n");
 return 0;
