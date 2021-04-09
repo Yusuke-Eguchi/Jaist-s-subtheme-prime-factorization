447a9d15cf1e964274bdb42a8896d4329da1fef5
Yusuke-Eguchi <yusuke499@gmail.com>
2021-04-09 14:23:20 +0900

    更新

--- soinsu2.cu
+++ soinsu2.cu
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 
-#define target 2*3*5*10^50
+#define target 2*2*2*3*3*3*5*5*5
 
-__device__ int GCD(long int *a, long int *b)
+__device__ int GCD(int *a, int *b)
 {
-	long int c;
+	int c;
 	if(*a == 0){
 		return *b;
 	} else {
@@ -14,13 +14,12 @@ __device__ int GCD(long int *a, long int *b)
 	}
 }
 
-__global__ void kernel(long int *A, int *d_B)
+__global__ void kernel(int *A, int *d_B)
 {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	int j = blockIdx.y * blockDim.y + threadIdx.y;
 	int k;
-	int flag = 0;
-	long int a = i - j, b;
+	int a = i - j, b, flag = 0;
 	if(i >= __powf(*A,0.5) + 1 && j >= __powf(*A,0.5) + 1 && a > 1 && i < *A && j < *A){
 		if(i^2 % *A == j^2 % *A){
 			b = GCD(&a, A);
@@ -37,20 +36,21 @@ __global__ void kernel(long int *A, int *d_B)
 }
 
 int main(){
-    long int *d_target, A = target;
-	long int *d_B, B[target*target];
+    int *d_target, A = target;
+	int *d_B;
+	static int B[target*target];
 	int i, j;
 	for(i=0;i<A*A;i++){
 		B[i] = 0;
 	}
-    cudaMalloc((void**)&d_target,sizeof(long int));
-	cudaMalloc((void**)&d_B,sizeof(long int)*A*A);
-	cudaMemcpy(d_target,&A,sizeof(long int),cudaMemcpyHostToDevice);
-	cudaMemcpy(d_B,&B,sizeof(long int)*A*A,cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&d_target,sizeof(int));
+	cudaMalloc((void**)&d_B,sizeof(int)*A*A);
+	cudaMemcpy(d_target,&A,sizeof(int),cudaMemcpyHostToDevice);
+	cudaMemcpy(d_B,&B,sizeof(int)*A*A,cudaMemcpyHostToDevice);
 	dim3 block(32,32);
 	dim3 grid((A+31)/32,(A+31)/32);
 	kernel<<<grid,block>>>(d_target,d_B);
-	cudaMemcpy(&B,d_B,sizeof(long int)*A*A,cudaMemcpyDeviceToHost);
+	cudaMemcpy(&B,d_B,sizeof(int)*A*A,cudaMemcpyDeviceToHost);
 	cudaFree(d_target);
 	cudaFree(d_B);	
 	for(i=0;i<A*A;i++){
